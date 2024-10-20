209a2a363c1576528fc20034cf43688cd57047fb
filipCurcic <filipcurcic42@gmail.com>
2024-10-20 21:17:37 +0200

    Add basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,30 @@
+import { Board } from './components/Board';
+import { Stack } from './components/common/Stack';
+
+import { useTokens } from './theme/tokens';
+
 function App() {
-  return <></>;
+  const tokens = useTokens();
+  return (
+    <Stack
+      center
+      style={{
+        width: '100vw',
+        height: '100vh',
+      }}
+    >
+      <Stack
+        vertical
+        style={{
+          width: '60%',
+          height: '100%',
+          background: tokens.colors.white,
+        }}
+      >
+        <Board />
+      </Stack>
+    </Stack>
+  );
 }
 
 export default App;
---
+++ src/components/TicketsContainer.tsx
@@ -0,0 +1,39 @@
+import { Stack } from './common/Stack';
+import { TicketItem } from './TicketItem';
+
+import { type Ticket, type Status } from '../types/SharedTypes';
+
+import { useTokens } from '../theme/tokens';
+
+type TicketsContainerProps = {
+  statusData: Status;
+  tickets: Ticket[];
+};
+
+export const TicketsContainer = ({
+  statusData,
+  tickets,
+}: TicketsContainerProps) => {
+  const tokens = useTokens();
+
+  return (
+    <Stack
+      vertical
+      css={{
+        width: '100%',
+        backgroundColor: tokens.colors[statusData.containerColor],
+        minHeight: '450px',
+      }}
+      align="center"
+      padding="small-lg"
+      gap="small-lg"
+    >
+      <TicketItem
+        title={'ticket.title'}
+        id={'ticket.id'}
+        key={'ticket.id'}
+        backgroundColor={statusData.ticketColor}
+      />
+    </Stack>
+  );
+};
