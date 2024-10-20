e7941edd0a0f2cd9b37ad7e4da7a36dda5155e16
filipCurcic <filipcurcic42@gmail.com>
2024-10-20 18:59:27 +0200

    Add typography component

---
+++ src/components/common/Typography/Typography.types.ts
@@ -0,0 +1,34 @@
+import { Tokens } from '../../../theme/tokens';
+
+export type PolymorphicRef<C extends React.ElementType> =
+  React.ComponentPropsWithRef<C>['ref'];
+
+type PolymorphicComponentPropWithRef<
+  C extends React.ElementType,
+  Props = object
+> = PolymorphicComponentProp<C, Props> & { ref?: PolymorphicRef<C> };
+
+type AsProp<C extends React.ElementType> = {
+  as?: C;
+};
+type PropsToOmit<C extends React.ElementType, P> = keyof (AsProp<C> & P);
+
+type PolymorphicComponentProp<
+  C extends React.ElementType,
+  Props = object
+> = React.PropsWithChildren<Props & AsProp<C>> &
+  Omit<React.ComponentPropsWithoutRef<C>, PropsToOmit<C, Props>>;
+
+export type TypographyProps<C extends React.ElementType> =
+  PolymorphicComponentPropWithRef<
+    C,
+    {
+      size?: keyof Tokens['size'];
+      color?: keyof Tokens['colors'];
+      emphasis?: keyof Tokens['emphasis'];
+    }
+  >;
+
+export type TypographyComponent = <C extends React.ElementType = 'p'>(
+  props: TypographyProps<C>
+) => React.ReactElement;
