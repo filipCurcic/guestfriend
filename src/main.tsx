86cfb28d792f63925faea8bb6cfe22928b7da47f
filipCurcic <filipcurcic42@gmail.com>
2024-10-20 23:49:46 +0200

    Add ticket context

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { TicketContextProvider } from './context/TicketContext.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <TicketContextProvider>
+      <App />
+    </TicketContextProvider>
+  </StrictMode>
+);
